16280689ea453fbf5f77bdb4ceed3c2f57d59a0f
IsaacCooke <cookeisaac2@gmail.com>
2024-04-09 21:52:11 +0100

    Made progress?

--- site/src/lib/api/query.ts
+++ site/src/lib/api/query.ts
@@ -1,6 +1,30 @@
-import {onMount} from 'svelte';
+import { onMount } from "svelte";
 
-const getAllArticles() {
+const getAllArticles = async () => {
   let articles;
+  let query = `
+  {
+    getAllArticles {
+      ID
+      Content
+      Title
+      Length
+      DateUploaded
+      ReadCount
+      ThumbnailUrl
+      Writer {
+        ID
+        FirstName
+        LastName
+        ProfileUrl
+      }
+    }
+  }`;
 
-}
+  const response = await fetch("https://localhost:8080/graphql", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ query }),
+  });
+  articles = response.data;
+};
